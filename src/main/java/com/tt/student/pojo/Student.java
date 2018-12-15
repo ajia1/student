beceb41de13d29573c54f19aff6f7daabc5e36f7
ajia1 <15234714082@163.com>
2018-12-15 08:56:25 +0800

    这是封装属性

--- src/main/java/com/tt/student/pojo/Student.java
+++ src/main/java/com/tt/student/pojo/Student.java
@@ -2,4 +2,12 @@ package com.tt.student.pojo;
 
 public class Student {
     private String login;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
 }
